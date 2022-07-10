7b5b4e6bcf2e4a6dbb038cd0e3f94cb3a05b7b74
aaculp <aaculp@icloud.com>
2022-07-10 12:32:10 -0400

    saving changes

--- src/homeScreen/index.js
+++ src/homeScreen/index.js
@@ -10,7 +10,13 @@ const HomeScreen = () => {
   return (
     <div>
       <div>Quiz</div>
-      <Link to="/middleScreen">Start!</Link>
+      <Link 
+        to="/middleScreen" 
+        state={{
+          // this is where i will add state to pass on
+        }}
+        onClick={() => console.log("just clicked me")}
+      >Start!</Link>
     </div>
   )
 }
